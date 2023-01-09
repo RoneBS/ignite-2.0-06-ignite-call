d60570df6f01a555b995d0fa3389b2c899f0e6c1
Rone Silveira <ronebs10@hotmail.com>
2023-01-09 14:05:58 -0300

    cadastro-de-usuario: Generating cookies when creating user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -48,6 +49,10 @@ const Register = () => {
         username: data.username,
       })
     } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+      }
+
       console.log(err)
     }
   }
